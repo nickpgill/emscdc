9ff40a9ac88230c5175357c6c918b770b1acfda1
Nick Gill <chatwithgill@gmail.com>
2018-02-23 11:57:17 +0000

    slowly slowly

---
+++ erce_uca.php
@@ -0,0 +1,35 @@
+
+<html><head>
+
+ 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>EMS-CDC The European Mathematical Society Committee for Developing Countries</title>
+
+<LINK rel="stylesheet" href="style.css" type="text/css" title="style">
+
+<body id=iasbspage>
+
+<?php include("includes/topbit.php"); ?>
+
+
+
+<h2>Probability and Statistics, Université Cadi Ayyad</H2>
+
+<p align=center>
+<img src = "uca.jpg" width=600><br>
+Based in Marrakech, Morocco, this department received the ERCE label in 2016.
+</p>
+<p>More information on the department of mathematics can be found <a href = "https://sites.google.com/a/uca.ma/statistics-and-probability-seminar-marrakesh-group/home">at their webpage</a>.
+</p>
+
+
+
+
+<?php include("includes/bottombit.php"); ?>
+
+
+</body>
+</html>
---
+++ twinning.php
@@ -0,0 +1,96 @@
+
+<html><head>
+
+ 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>EMS-CDC The European Mathematical Society Committee for Developing Countries</title>
+
+<LINK rel="stylesheet" href="style.css" type="text/css" title="style">
+
+<body id=activitiespage>
+
+<?php include("includes/topbit.php"); ?>
+
+
+
+
+<h3>
+Offer of seed grants by the Committee for Developing Countries
+</h3>
+
+<p>
+&nbsp; &nbsp; 
+A number of initiatives are currently under way to address the challenge of supporting mathematics in the developing world.
+</p><p>&nbsp; &nbsp; 
+In particular, our committee EMS-CDC is impressed by a scheme of the <a href="http://math.golonka.se/">International Science Program ISP </a>
+at Uppsala Universiy, initiated by one of our members 
+<a href="http://math.golonka.se/">Anders Wandahl</a>, whereby a department in the developed world "twins" with a department in the developing world, to help them in various ways, including paying for subscriptions to mathematical databases. What has been done so far by our Swedish colleagues can be found on the site  
+<a href="http://math.golonka.se/">"e-Math for Africa"</a> 
+</p><p>
+&nbsp; &nbsp; 
+Another effort towards helping developing countries to access literature and to sharpen the links with colleagues from  institutions in developed countries has been initiated by the 
+<a href="http://smf.emath.fr/en/Adhesions/ParrainagePED/">Société Mathématique de France (SMF)</a>. Under this program, laboratories or institutes with enough funds will support active mathematical centres with less financial support by offering them a registration as an institutional member of <a href="http://smf.emath.fr/en/Adhesions/ParrainagePED/">SMF</a>, and the <a href="http://smf.emath.fr/en/Adhesions/ParrainagePED/">SMF</a> will offer a subscription to one of its journals. The idea is that such a process is part of a cooperation between the two institutions, and that eventually after a few years the institution from the South should be in a position to subscribe itself to <a href="http://smf.emath.fr/en/Adhesions/ParrainagePED/">SMF</a>.
+</p><p>
+&nbsp; &nbsp; 
+We wish to support this scheme, which we call "twinning", in every possible way, because we think it involves directly connecting mathematicians on a one-to-one basis, and once links are forged they can be easily sustainable in the long term at no great initial cost.
+</p><p>
+&nbsp; &nbsp; 
+Although the CDC does not have much funding, we  have launched this project by offering, during an initial period, a kind of "seed grant" of up to 500 euros, to any department or institution in the developed world which wishes to participate in such a twinning scheme and is willing to provide at least the same amount in support.  We hope that such an initial grant will introduce mathematicians to undertake some very worthwhile work which they will find it satisfying to continue afterwards. Interested departments or individuals in the developed world should address any of us below in the first instance. 
+</p><p>
+&nbsp; &nbsp; 
+Here is a concrete example: a student from Ho Chi Minh City received a support from 500 Euros under this program; this money came in addition to funds from the 
+<a href="http://www.institut.math.jussieu.fr/">Institut de Mathématiques de Jussieu</a> and from the 
+<a href="http://www.cnrs.fr/">CNRS</a>; each of the amounts was quite low, but altogether this enabled her to spend three months in Paris and attend a number of courses of the Master 2 program. After this stay she got a scholarship and she is now preparing a thesis in Canada. 
+</p><p>
+&nbsp; &nbsp; 
+The list of institutions which participate so far is not very long, but it is a beginning!  And we hope colleagues from other European countries will join in the scheme. We would certainly like to see this scheme broadened, both in scope and in the geographical areas involved. The activities we have in mind for the twins to share could include:
+<ol>
+<li>Help with journal and database subscriptions, as detailed above
+<li><a href="donation.php">Donation</a> of books, journals and equipment
+<li>Supplying guest lecturers and supporting visits from the less developed department
+<li><a href="http://www.lms.ac.uk/content/mentoring-african-research-mathematics">Mentoring African Research Mathematics (MARM)</a>, a London Mathematical Society scheme.
+<li>Support for attending major conferences: this we find very important for the career of individual mathematicians in the developing world.
+</ol>
+</p><p>
+&nbsp; &nbsp; 
+There is no doubt that, as the scheme matures, more activities will be found beneficial.  The ultimate aim is be that the elder twin will take care of the younger twin, in the real sense of "twins": the elder has the younger's mathematical well-being at heart and will come to its help whenever necessary.  Thus the younger twin will have somebody to "turn to" in case of need, ... ideally until no such needs remain as a result of global progress!
+</p><p>
+&nbsp; &nbsp; 
+On behalf of the Committee for Developing Countries, European Mathematical Society:
+<i>
+<br />
+
+&nbsp; &nbsp; &nbsp; &nbsp; 
+<a href="http://www.math.uu.se/~leifab/isp/">Leif Abrahamsson</a>
+<br />
+
+&nbsp; &nbsp; &nbsp; &nbsp; 
+Lars Dovling Andersen,  
+<br />
+
+&nbsp; &nbsp; &nbsp; &nbsp; 
+<a href="http://folk.uio.no/giulian/">Giulia di Nunno</a> (Chair of the subcommittee) 
+<br />
+
+
+&nbsp; &nbsp; &nbsp; &nbsp; 
+<a href="http://www.maths.ox.ac.uk/~tsou/">Tsou Sheung Tsun</a>
+<br />
+
+&nbsp; &nbsp; &nbsp; &nbsp; 
+<a href="http://www.math.jussieu.fr/~miw/">Michel Waldschmidt</a>
+<br />
+
+&nbsp; &nbsp; &nbsp; &nbsp; 
+<a href="http://math.golonka.se/">Anders Wandahl</a>
+ </i>
+
+
+<?php include("includes/bottombit.php"); ?>
+
+
+</body>
+</html>
